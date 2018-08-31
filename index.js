9653c79f744ddfacf3de0c1562ebf8a00b5b5ea4
Cara D <ephemere@protonmail.com>
2018-08-31 03:33:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
     return "I love you, too." 
   } else if (string === lowercase ){
     return "I can't hear you!"
-  // } else (string === uppercase){
-  //   return "YES INDEED!"
+  } else if (string === uppercase){
+    return "YES INDEED!"
   }
 }
